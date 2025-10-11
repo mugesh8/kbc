a5d2bc5dea2322e779ba968cf33e86c164e89c16
mugesh8 <mugeshkumar.deecodes@gmail.com>
2025-10-11 16:03:40 +0530

    QC changes

--- src/components/User/Category.js
+++ src/components/User/Category.js
@@ -307,7 +307,7 @@ const BusinessCategories = () => {
           {/* Search Section - Only show when not in business view */}
           {!showBusinesses && (
             <div className="max-w-4xl mx-auto bg-white rounded-xl sm:rounded-2xl shadow-xl p-4 sm:p-6">
-              <div className="relative">
+              <div className="relative border border-gray-200 rounded-xl">
                 <div className="absolute inset-y-0 left-0 pl-3 sm:pl-4 flex items-center pointer-events-none">
                   <Search className="h-4 w-4 sm:h-5 sm:w-5 text-gray-400" />
                 </div>
